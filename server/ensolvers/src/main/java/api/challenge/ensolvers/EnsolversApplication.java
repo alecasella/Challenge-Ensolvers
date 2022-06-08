d55bb6411bc0a5fc5cc98243412a97e02b42fada
Alessandro Casella <javi_casella95@outlook.com.ar>
2022-06-08 09:02:28 -0300

    repository start, added folder for front and back

---
+++ server/ensolvers/src/main/java/api/challenge/ensolvers/EnsolversApplication.java
@@ -0,0 +1,13 @@
+package api.challenge.ensolvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsolversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsolversApplication.class, args);
+	}
+
+}
---
+++ server/ensolvers/src/test/java/api/challenge/ensolvers/EnsolversApplicationTests.java
@@ -0,0 +1,13 @@
+package api.challenge.ensolvers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsolversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
